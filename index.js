6d787877deae8caca8a0030c54af03285ee97819
Tina <tina.jston@gmail.com>
2020-03-04 10:21:15 +0000

    routing in prod

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
